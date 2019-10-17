80696ed339c50a8f4ce002a9e24e81e94c46dff6
Matthew Clark <kitsune361@disaster-area-productions.com>
2019-10-16 23:32:36 -0500

    Dockerfile changes: Aimed npm install at my copy of app. Added ENV variables to control verbose logging and hybrid mode

--- docker/init.sh
+++ docker/init.sh
@@ -9,5 +9,26 @@ sleep 10
 echo "\n\n++++++++++ Re checking IP ++++++++++ "
 curl -s ifconfig.co
 
+# build args
+if [[ -z "$HYBRID" ]]; then
+  HYBRID="false"
+fi
+
+if [[ -z "$VERBOSE" ]]; then
+  VERBOSE="false"
+fi
+
+ARGS="-l 0.0.0.0 -t /tf -d /data"
+
+if [[ ${VERBOSE,,} = "true" ]]; then
+echo "\n\n++++++++++ Enabling Verbose Mode ++++++++++ "
+  ARGS="$ARGS -v"
+fi  
+
+if [[ ${HYBRID,,} = "true" ]]; then
+echo "\n\n++++++++++ Enabling Hybrid Mode ++++++++++ "
+  ARGS="$ARGS -y"
+fi
+
 echo "\n\n++++++++++ Starting torrent engine ++++++++++ "
-webtorrent-webui -l 0.0.0.0 -t /tf -d /data
+webtorrent-webui $ARGS
