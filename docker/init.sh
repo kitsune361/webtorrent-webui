2580e67afe5c442da00e7acfbfa3c7b2098012bd
Matthew Clark <kitsune361@disaster-area-productions.com>
2019-10-17 19:47:52 -0500

    Docker fixes: FIXED: Streamlined use of environment variables in init.sh script. FIXED: Aimed NPM at my git copy of the app.

--- docker/init.sh
+++ docker/init.sh
@@ -8,24 +8,16 @@ sleep 10
 
 echo "\n\n++++++++++ Re checking IP ++++++++++ "
 curl -s ifconfig.co
-
+ 
 # build args
-if [[ -z "$HYBRID" ]]; then
-  HYBRID="false"
-fi
-
-if [[ -z "$VERBOSE" ]]; then
-  VERBOSE="false"
-fi
-
 ARGS="-l 0.0.0.0 -t /tf -d /data"
 
-if [[ ${VERBOSE,,} = "true" ]]; then
+if [ "${VERBOSE:-false}" = "true" ]; then
 echo "\n\n++++++++++ Enabling Verbose Mode ++++++++++ "
   ARGS="$ARGS -v"
-fi  
+fi
 
-if [[ ${HYBRID,,} = "true" ]]; then
+if [ "${HYBRID:-false}" = "true" ]; then
 echo "\n\n++++++++++ Enabling Hybrid Mode ++++++++++ "
   ARGS="$ARGS -y"
 fi
